6413270ca99208bd9c650cf5e40feae21df19604
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-04-25 18:05:35 +0100

    hashbrown

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,5 +1,5 @@
-use std::collections::hash_map::Entry;
-use std::collections::{BTreeMap, HashMap};
+use hashbrown::{HashMap, hash_map::Entry};
+use std::collections::BTreeMap;
 use std::hint;
 use std::io;
 use std::io::Write;

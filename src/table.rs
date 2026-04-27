9dd64888e120fe2290dc9466e0eae0e2cc1ef898
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-04-27 22:15:30 +0100

    vai

--- src/table.rs
+++ src/table.rs
@@ -1,43 +1,18 @@
 use std::hash::{BuildHasher, Hash};
 
-// pub enum Entry {
-//     Vacant,
-//     Occupied,
-// }
-
-// pub struct VacantEntry<'a, K, V, H, const S: usize>
-// where
-//     K: Hash,
-//     H: BuildHasher,
-// {
-//     hash: u64,
-//     key: K,
-//     table: &'a mut Table<K, V, H, S>,
-// }
-
-// pub struct OccupiedEntry<'a, K, V, H, const S: usize>
-// where
-//     K: Hash,
-//     H: BuildHasher,
-// {
-//     hash: u64,
-//     elem: (K, V),
-//     table: &'a mut Table<K, V, H, S>,
-// }
-
-#[derive(Debug)]
-pub struct Table<K, V, H, const S: usize> {
-    hash_builder: H,
-    mask: u64,
-    table: [Option<(K, V)>; S],
-}
-
 fn make_hash<Q, H>(hash_builder: &H, val: &Q) -> u64
 where
     Q: Hash,
     H: BuildHasher,
 {
     hash_builder.hash_one(val)
+}
+
+#[derive(Debug)]
+pub struct Table<K, V, H, const S: usize> {
+    hash_builder: H,
+    mask: usize,
+    table: [Option<(K, V)>; S],
 }
 
 impl<K, V, H, const S: usize> Table<K, V, H, S>
@@ -48,36 +23,48 @@ where
     pub fn with_hasher(hasher: H) -> Self {
         Self {
             hash_builder: hasher,
-            mask: S as u64 - 1,
+            mask: S - 1,
             table: [const { None }; S],
         }
     }
 
-    pub fn insert(&self, key: K, value: V) -> Option<V> {
-        let hash = make_hash(&self.hash_builder, &key);
+    pub fn get(&self, key: K) -> Option<&V> {
+        let hash = make_hash(&self.hash_builder, &key) as usize;
+        let index = hash & self.mask;
+        let elem = self.table[index].as_ref();
+
+        elem.and_then(|el| Some(&el.1))
+    }
+
+    pub fn get_mut(&mut self, key: K) -> Option<&mut V> {
+        let hash = make_hash(&self.hash_builder, &key) as usize;
+        let index = hash & self.mask;
+        let elem = self.table[index].as_mut();
+
+        elem.and_then(|el| Some(&mut el.1))
+    }
+
+    pub fn insert(&mut self, key: K, value: V) {
+        let hash = make_hash(&self.hash_builder, &key) as usize;
         let index = hash & self.mask;
 
-        dbg!(index);
-
-        None
+        self.table[index] = Some((key, value));
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::metrics::Aggregate;
     use std::hash::RandomState;
 
     #[test]
     fn insert() {
-        let table =
-            Table::<u8, &'static str, RandomState, 10_000>::with_hasher(RandomState::default());
+        let mut table =
+            Table::<&[u8], Aggregate, RandomState, 10_000>::with_hasher(RandomState::default());
 
-        // dbg!(&table);
-        assert_eq!(table.insert(0, "u8::MIN"), None);
-        // dbg!(&table);
-
-        assert_eq!(table.insert(255, "u8::MAX_OLD"), None);
-        assert_eq!(table.insert(255, "u8::MAX_NEW"), Some("u8::MAX_OLD"));
+        dbg!(&table);
+        table.insert(b"jac", Aggregate::new(1));
+        dbg!(&table);
     }
 }
